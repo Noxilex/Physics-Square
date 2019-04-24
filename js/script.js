84f99843bb35fdd06990c16062143f9640cfde64
Noxilex <dhoop59@hotmail.fr>
2019-04-24 16:48:34 +0200

    Changed code to be able to reset using console

--- js/script.js
+++ js/script.js
@@ -1,97 +1,109 @@
-(function() {
+/*** Drawing ***/
 
-	/*** Drawing ***/
+c.width = width;
+c.height = height;
+
+var resizeCanvas = function(){
+	width = window.innerWidth;
+	height = window.innerHeight;
 
 	c.width = width;
 	c.height = height;
-
-	var resizeCanvas = function(){
-		width = window.innerWidth;
-		height = window.innerHeight;
-
-		c.width = width;
-		c.height = height;
-		draw();
-	}
-	var mousePosition = null;
-	var getMousePosAndApplyIt = function(evt){
-		mousePosition = getMousePos(c, evt);
-		if(light.state == "CONTROL"){
-			light.applyMousePos(mousePosition);
-			for(var i = 0; i < squares.length; i++){
-				squares[i].getMousePos(mousePosition);
-			}
+	draw();
+}
+var mousePosition = null;
+var getMousePosAndApplyIt = function(evt){
+	mousePosition = getMousePos(c, evt);
+	if(light.state == "CONTROL"){
+		light.applyMousePos(mousePosition);
+		for(var i = 0; i < squares.length; i++){
+			squares[i].getMousePos(mousePosition);
 		}
 	}
+}
 
-	var prepareLaunch = function(evt){
-		light.cancelForce();
-		console.log("prepareFire");
-		light.initialPosition = getMousePos(c, evt);
-	}
+var prepareLaunch = function(evt){
+	light.cancelForce();
+	console.log("prepareFire");
+	light.initialPosition = getMousePos(c, evt);
+}
 
-	var launchLight = function(evt){
-		console.log("Launch !")
-		light.finalPosition = getMousePos(c, evt);
-		var force_x = light.initialPosition.x - light.finalPosition.x;
-		var force_y = light.initialPosition.y-light.finalPosition.y;
-		var forceVector = new Vector(force_x, force_y);
-		console.log(forceVector)
-		forceVector.multiply(0.1);
-		console.log(forceVector)
-		light.applyForce(forceVector);
-	}
+var launchLight = function(evt){
+	console.log("Launch !")
+	light.finalPosition = getMousePos(c, evt);
+	var force_x = light.initialPosition.x - light.finalPosition.x;
+	var force_y = light.initialPosition.y-light.finalPosition.y;
+	var forceVector = new Vector(force_x, force_y);
+	console.log(forceVector)
+	forceVector.multiply(0.1);
+	console.log(forceVector)
+	light.applyForce(forceVector);
+}
 
-	window.addEventListener('resize', resizeCanvas, false);
-	c.addEventListener('mousemove', getMousePosAndApplyIt, false);
-	c.addEventListener('mousedown', prepareLaunch, false);
-	c.addEventListener('mouseup', launchLight, false);
+window.addEventListener('resize', resizeCanvas, false);
+c.addEventListener('mousemove', getMousePosAndApplyIt, false);
+c.addEventListener('mousedown', prepareLaunch, false);
+c.addEventListener('mouseup', launchLight, false);
 
-	/*** Model ***/
-	var rows = width/SQUAREWIDTH;
-	var columns = height/SQUAREWIDTH;
-	var squares = [];
+/*** Model ***/
+
+var rows;
+var columns;
+var squares;
+var light = new Light(ctx, null, null);
+var setup = function(){
+	rows = width/SQUAREWIDTH;
+	columns = height/SQUAREWIDTH;
+	squares = [];
 	for(var i = 0; i < rows; i++){
 		for(var j = 0; j < columns; j++){
 			squares.push(new Square(ctx,i*SQUAREWIDTH, j*SQUAREWIDTH, SQUAREWIDTH));
 		}
 	}
-	var light = new Light(ctx, null, null);
+	light = new Light(ctx, null, null);
 	light.getImage();
+}
 
-	/*** Update ***/
+setup();
 
-	var requestAnimationFrame = window.requestAnimationFrame || 
-	window.mozRequestAnimationFrame ||
-	window.webkitRequestAnimationFrame ||
-	window.msRequestAnimationFrame;
+/*** Update ***/
 
-	var update = function(){
-		clean();
+var requestAnimationFrame = window.requestAnimationFrame || 
+window.mozRequestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.msRequestAnimationFrame;
 
-		light.update();
+var currentRequestAnimation;
 
-		for(var i = 0; i < squares.length; i++){
-			if(light.state == "FREE"){
-						squares[i].getMousePos({x:light.x, y:light.y});
-			}
-			squares[i].update();
+var update = function(){
+	clean();
+
+	light.update();
+
+	for(var i = 0; i < squares.length; i++){
+		if(light.state == "FREE"){
+					squares[i].getMousePos({x:light.x, y:light.y});
 		}
-		
-		draw()
-
-		requestAnimationFrame(update);
+		squares[i].update();
 	}
+	
+	draw()
 
-	var draw = function(){
+	requestAnimationFrame(update);
+}
 
-	}
+var reset = function(){
+	setup();
+}
 
-	var clean = function(){
-		/*ctx.fillStyle = "black";
-		ctx.fillRect(0, 0, c.width, c.height);*/
-		ctx.clearRect(0, 0, c.width, c.height);
-	}
+var draw = function(){
 
-	update();
-})();
+}
+
+var clean = function(){
+	/*ctx.fillStyle = "black";
+	ctx.fillRect(0, 0, c.width, c.height);*/
+	ctx.clearRect(0, 0, c.width, c.height);
+}
+
+update();
